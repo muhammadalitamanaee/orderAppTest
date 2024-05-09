212947fcdeb71833723316583a8d8b466143d1a8
Ali Tamanaei <alitaman200041@gmail.com>
2024-05-09 14:23:17 +0330

    fix: adding some unit tests for the checkBox and popover

---
+++ sundae-starter/src/pages/summary/test/SummaryForm.test.jsx
@@ -0,0 +1,56 @@
+import SummaryForm from "../SummaryForm";
+import { fireEvent, render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+describe("the test for the summary form ", () => {
+  test("the initail state of a btn and checkbox", () => {
+    render(<SummaryForm />);
+    const checkbox = screen.getByRole("checkbox", {
+      name: /Terms and Conditions/i,
+    });
+    const submitButton = screen.getByRole("button", {
+      name: /Confirm order/i,
+    });
+    expect(checkbox).toBeInTheDocument();
+    expect(checkbox).not.toBeChecked();
+    expect(submitButton).toBeDisabled();
+  }),
+    test("check box checked and bt is enabled", async () => {
+      render(<SummaryForm />);
+      const checkbox = screen.getByRole("checkbox", {
+        name: /Terms and Conditions/i,
+      });
+      const submitButton = screen.getByRole("button", {
+        name: /Confirm order/i,
+      });
+      const user = userEvent.setup();
+      expect(checkbox).toBeInTheDocument();
+      expect(checkbox).not.toBeChecked();
+      expect(submitButton).toBeDisabled();
+      await user.click(checkbox);
+      expect(checkbox).toBeChecked();
+      expect(submitButton).toBeEnabled();
+      await user.click(checkbox);
+      expect(checkbox).not.toBeChecked();
+      expect(submitButton).toBeDisabled();
+    });
+  test("popover response to hover", async () => {
+    render(<SummaryForm />);
+    const user = userEvent.setup();
+    //popover starts hidden
+    const nullPopOver = screen.queryByText(
+      /No ice cream will actually be delivered/i
+    );
+    expect(nullPopOver).not.toBeInTheDocument();
+    //popover appear on screen
+    const termsAndConditions = screen.getByText(/terms and conditions/i);
+    await user.hover(termsAndConditions);
+    const popover = screen.getByText(
+      /No ice cream will actually be delivered/i
+    );
+    expect(popover).toBeInTheDocument();
+
+    //popover goes away again
+    await user.unhover(termsAndConditions);
+    expect(popover).not.toBeInTheDocument();
+  });
+});
